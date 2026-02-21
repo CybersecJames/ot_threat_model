e1aab213a10e980176891733638d27c8aab55a80
James Montrief <james.montrief@gmail.com>
2026-02-21 16:02:05 -0500

    added config block

--- iot_intel.py
+++ iot_intel.py
@@ -2,6 +2,9 @@ from mitreattack.stix20 import MitreAttackData
 from pyvis.network import Network
 import networkx as nx
 
+# --- Configuration options ---
+output_file_name = "output_graph.html"
+
 # --- MITRE dataset ---
 dataset = "ics-attack.json"
 
@@ -18,7 +21,7 @@ def graph():
         G.add_node(
             group_name,
             group="group",
-            title='test run',
+            title='group',
             shape='square'
         )
         
@@ -28,7 +31,7 @@ def graph():
             G.add_node(
                 software_name,
                 group="software",
-                title='hmmmm',
+                title='software',
                 shape='dot'
             )
     
@@ -44,7 +47,7 @@ def graph():
 def write_html(graph):
     net = Network(directed=True)
     net.from_nx(graph)
-    net.write_html("maybeeee.html")
+    net.write_html(output_file_name)
     print("done")
  
 # --- Main function ---
