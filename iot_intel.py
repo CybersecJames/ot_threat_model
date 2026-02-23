772e1af9e4e39ecf6608c619b4c82fd1529c0a1e
James M <montriefj@michigan.gov>
2026-02-23 14:59:39 -0500

    added connections for techniques used by groups, fill tooltip, and added trextwrap as a dependency

--- iot_intel.py
+++ iot_intel.py
@@ -1,6 +1,7 @@
 from mitreattack.stix20 import MitreAttackData
 from pyvis.network import Network
 import networkx as nx
+from textwrap import fill
 
 # --- Configuration options ---
 output_file_name = "output_graph.html"
@@ -11,9 +12,14 @@ dataset = "ics-attack.json"
 # --- Get all threat groups from MITRE ATT&CK ---
 mitre_attack_data = MitreAttackData(dataset)
 
+def get_groups():
+    groups = mitre_attack_data.get_groups()
+    return groups
+
 # --- Define the graph, add nodes and edges ---
 def graph():
     groups = mitre_attack_data.get_groups()
+    
     
     G = nx.DiGraph()
     for group in groups:
@@ -24,6 +30,22 @@ def graph():
             title='group',
             shape='square'
         )
+        
+        techs = mitre_attack_data.get_techniques_used_by_group(group_stix_id=group["id"])
+        
+        for t in techs:
+            tech_name = t["object"]["name"]
+            G.add_node(
+                tech_name,
+                group="tech",
+                title=fill(t["object"]['description'], 50),
+                shape='square'
+            )
+            
+            G.add_edge(
+                group_name,
+                tech_name
+            )
         
         software = mitre_attack_data.get_software_used_by_group(group['id'])
         for s in software:
